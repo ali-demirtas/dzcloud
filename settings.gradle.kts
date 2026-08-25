52d2d9fc40de4c7bbcb4dd840dd20c85a6557ef8
Ali Demirtas <alidemirtas94@gmail.com>
2026-08-25 14:12:23 +0300

    Implement resolution strategy for Cloudstream plugin
    Added resolution strategy for specific plugin version.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,23 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://jitpack.io")
     }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.lagradost.cloudstream3.gradle") {
+                useModule("com.github.recloudstream:gradle:-SNAPSHOT")
+            }
+        }
+    }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.name = "dzcloud"
 include(":DizipalProvider")
