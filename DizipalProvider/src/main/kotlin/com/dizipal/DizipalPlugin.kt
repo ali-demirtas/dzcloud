f2c80d20c9c71b4ec04e9828cc5947c8ebce1795
Ali Demirtas <alidemirtas94@gmail.com>
2026-08-25 13:59:39 +0300

    Add DizipalPlugin class for Cloudstream integration

---
+++ DizipalProvider/src/main/kotlin/com/dizipal/DizipalPlugin.kt
@@ -0,0 +1,13 @@
+package com.dizipal
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class DizipalPlugin: Plugin() {
+    override fun load(context: Context) {
+        // Provider sınıfınızı buraya kaydedin
+        registerMainAPI(DizipalProvider())
+    }
+}
