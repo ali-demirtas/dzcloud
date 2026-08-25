ffaeaea7576a5e1a8f733498dc955cf028e269bd
ali-demirtas <alidemirtas94@gmail.com>
2026-08-25 12:22:50 +0000

    Deploying to builds from @ ali-demirtas/dzcloud@be0bbc7c117787e64fe065ecf8253ca76180ca16 🚀

--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-}
-
-tasks.register<Delete>("clean") {
-    delete(layout.buildDirectory)
-}
